1bffcbbcf46ddb0e578ae2f11a865871a9101166
NamoxKalvium <Namonarayan.Mina@Kalvium.community>
2025-02-25 10:35:45 +0530

    done

--- src/components/bookcard.jsx
+++ src/components/bookcard.jsx
@@ -1,1 +1,15 @@
-// write the book component code here
+function BookCard({ book }) {
+    return (
+      <div className="book-card">
+        <div className="image-box">
+          <img src={book.image} alt={book.name} className="book-image" />
+        </div>
+        <h3>{book.name}</h3>
+        <p><strong>Genre:</strong> {book.genre}</p>
+        <p><strong>Author:</strong> {book.author}</p>
+      </div>
+    );
+  }
+  
+  export default BookCard;
+  
